a5a07ee3fbc5649c33d1737d55d24ae4f6866cbd
Andy <andybarajas0418@gmail.com>
2020-07-29 14:35:44 -0400

    go fmt

--- main.go
+++ main.go
@@ -4,18 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main(){
+func main() {
 	r := gin.Default()
-	r.GET("/menu",func(c *gin.Context){
-		c.File("./static/lunch.jpg")
-		c.File("./static/pg1.jpg")
-		c.File("./static/pg2.jpg")
-		c.File("./static/pg3.jpg")
-		c.File("./static/pg4.jpg")
-		c.File("./static/pg5.jpg")
-		c.File("./static/pg6.jpg")
-		c.File("./static/pg7.jpg")
-	})
+
+	r.Static("/menu", "./static/")
 
 	r.Run(":8080")
 }
